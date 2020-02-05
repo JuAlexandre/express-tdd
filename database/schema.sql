c82815a0bdca002206e4124fda8fb298d3a55abb
Julien <julien.alexandre26@gmail.com>
2020-02-05 08:56:38 +0100

    Updating the schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,17 @@
 -- database/schema.sql
+
+-- Development database
+CREATE DATABASE bookmarks_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS bookmark (
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   url VARCHAR(255),
   title VARCHAR(255)
-);
+) ENGINE = InnoDB;
 
+-- Tests database
+CREATE DATABASE bookmarks_app_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS bookmark (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  url VARCHAR(255),
+  title VARCHAR(255)
+) ENGINE = InnoDB;
